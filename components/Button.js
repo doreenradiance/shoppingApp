8583196061efcf9585a34eb8e0c07da33fe33be3
Doreen Mensah <doreenradiance@gmail.com>
2020-11-25 16:10:02 +0000

    changed Folder

--- components/Button.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Text,TouchableOpacity } from 'react-native';
-
-export default Button = ({ onPress, title, buttonStyle, textStyle }) => (
-    <TouchableOpacity onPress={onPress} style={buttonStyle}>
-      <Text style={textStyle}>{title}</Text>
-    </TouchableOpacity>
-)
