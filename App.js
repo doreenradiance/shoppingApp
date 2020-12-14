504d452f507e2e9427f5b9e576e46ec316d88098
Doreen Mensah <doreenradiance@gmail.com>
2020-12-14 10:42:19 +0000

    Made changes with the styles

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import { createStackNavigator } from '@react-navigation/stack'
 import AddToCart from './src/screens/AddToCart';
 import OnlineShopping from './src/screens/OnlineShopping';
 import PaymentSuccessful from './src/screens/PaymentSuccessful';
+import JacketsScreen from '../rn-eCommerce/src/screens/JacketsScreen';
 
 
 const Stack = createStackNavigator()
@@ -13,22 +14,21 @@ const Stack = createStackNavigator()
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator      
-      screenOptions={{
-        title:"Online Shopping",
+      <Stack.Navigator
+        screenOptions={{
+          title: "JacketsScreen",
           // headerStyle:{
           //   backgroundColor:"#3b31eb"
           // },
-          headerTintColor:"#3b31eb",
-          headerTitleAlign:"center",
-          headerRight:()=>(<Text style={{color:"#3b31eb", marginRight:20}}>Icons</Text>)
-      }}
+          headerTintColor: "#3b31eb",
+          headerTitleAlign: "center",
+          headerRight: () => (<Text style={{ color: "#3b31eb", marginRight: 20 }}>Icons</Text>)
+        }}
       >
-        <Stack.Screen 
-        // options={{}}
-        name='OnlineShopping' component={OnlineShopping} />
-        <Stack.Screen name='AddToCart' component={AddToCart} />
-        <Stack.Screen name='PaymentSuccessful' component={PaymentSuccessful} />
+        <Stack.Screen
+          // options={{}}
+          name='JacketsScreen' component={JacketsScreen} />
+        <Stack.Screen name='RedJacket' component={RedJacket} />
       </Stack.Navigator>
     </NavigationContainer>
 
